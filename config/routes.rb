d1dc20df2b31bcb7b1fe8f04a9572e87473ab94c
Japanesecedarbook <yositakasugi1113@gmail.com>
2020-06-11 13:16:14 +0900

    aaaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users
-=======
-  # devise_for :users
-  get 'messages/index'
->>>>>>> Stashed changes
   root "messages#index"
   resources :users, only: [:edit, :update]
 end
